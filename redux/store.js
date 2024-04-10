dd3511c691e3efb20ece503acd31a96d13cc547f
KolesnikovTimurWeb <109101206+KolesnikovTimurWeb@users.noreply.github.com>
2024-04-10 12:25:10 +0200

    Local saving Items

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
 import wishListSlice from "./slices/wishListSlice";
-
-export default configureStore({
+export const loadState = () => {
+   try {
+      const serializedState = localStorage.getItem("state");
+      if (serializedState === null) {
+         return undefined;
+      }
+      return JSON.parse(serializedState);
+   } catch (err) {
+      return undefined;
+   }
+};
+export const store = configureStore({
    reducer: {
       cart: cartSlice,
       wishList: wishListSlice,
-   }
+   },
+   preloadedState: loadState(),
 })
+
+store.subscribe(() => {
+   localStorage.setItem("state", JSON.stringify(store.getState()));
+});
